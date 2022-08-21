97c3e056c9d89c5e82bd59a2f697bfb97bdc0ce7
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2022-08-21 16:37:00 +0200

    fix: removed stories function

--- _11ty/plugins.js
+++ _11ty/plugins.js
@@ -1,16 +1,8 @@
-const { EleventyServerlessBundlerPlugin } = require('@11ty/eleventy');
 const feathericons = require('eleventy-plugin-feathericons');
 const pwa = require('@piraces/eleventy-plugin-pwa');
 const rss = require('@11ty/eleventy-plugin-rss');
 
 module.exports = [
-  [
-    EleventyServerlessBundlerPlugin,
-    {
-      name: 'stories',
-      functionsDir: 'netlify/functions',
-    },
-  ],
   [feathericons],
   [
     pwa,
